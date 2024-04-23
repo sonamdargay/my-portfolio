d56ce2ee149409a3582b40d1d82c8aeaa6daaaae
SvnvmD <someodargay167@gmail.com>
2024-04-23 13:06:57 +1000

    add .env example file

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,32 @@
-import {Container, Row, Col, Button, Image} from "react-bootstrap";
+import {Container, Row, Col, Button} from "react-bootstrap";
 import heroImage from '../assets/hero-image.png';
 import '../styles/styles.css'
 
-function Home(){
+function Home() {
     return (
-        <div className="hero-section d-flex align-items-center" style={{backgroundImage: `url(${heroImage})`}}>
-            <Container>
-                <Row className="justify-content-center">
-                    <Col md={8} className="text-center text-white">
-                        <h1>Welcome to My Portfolio</h1>
-                        <p></p>
-                        <Button variant="light" size="lg">Get Started</Button>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
+        // <div className="hero-section d-flex align-items-center" style={{backgroundImage: `url(${heroImage})`}}>
+        //     <Container>
+        //         <Row className="justify-content-center">
+        //             <Col md={8} className="text-center text-white">
+        //                 <h1>Welcome to My Portfolio</h1>
+        //                 <p></p>
+        //                 <Button variant="light" size="lg">Get Started</Button>
+        //             </Col>
+        //         </Row>
+        //     </Container>
+        // </div>
+
+    <div className="hero-section bg-primary text-light">
+        <Container>
+            <Row className="justify-content-center">
+                <Col md={8} className="text-center">
+                    <h1>Welcome to Your Portfolio</h1>
+                    <p>Your tagline or a brief description here</p>
+                    <Button variant="light" size="lg">Get Started</Button>
+                </Col>
+            </Row>
+        </Container>
+    </div>
     )
 }
 
